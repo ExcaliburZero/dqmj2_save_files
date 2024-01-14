30099ae9ac4d1cceb7a0323ab666e238ed67faf1
Christopher Wells <cwellsny@nycap.rr.com>
2024-01-14 14:37:51 -0800

    Restructure print statements

--- dqmj2_save_files/__main__.py
+++ dqmj2_save_files/__main__.py
@@ -10,7 +10,30 @@ from . import saves
 def describe(save_filepath: str) -> int:
     with open(save_filepath, "rb") as input_stream:
         data = saves.SaveData.from_sav(input_stream)
-        print(data)
+
+    print_header(data.header)
+    print_data(data.data)
+
+
+def print_header(header: saves.Header) -> None:
+    fields = [
+        ("Data checksum", header.data_checksum),
+        ("Header checksum", header.header_checksum),
+    ]
+
+    print("Header:")
+    for name, value in fields:
+        print(f"\t{name}: {value}")
+
+
+def print_data(data: saves.Data) -> None:
+    fields = [
+        ("Play time", data.play_time),
+    ]
+
+    print("Data:")
+    for name, value in fields:
+        print(f"\t{name}: {value}")
 
 
 if __name__ == "__main__":
